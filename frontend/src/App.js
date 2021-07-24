a01b2d7837252f4bdc1006f153216e8106e9c213
Emmanuel <emmanuelamon92@gmail.com>
2021-07-23 23:05:01 -0400

    Added form with form hook and stat hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,75 @@
-import React from 'react';
-import { Switch, Route, Link, useHistory } from 'react-router-dom';
-import PatientEntry from './components/PatientEntry';
-import './App.css';
+// <-- USING-FORM-HOOK-START -->
+import * as React from "react";
+import { useForm } from "react-hook-form";
+import Heading from "./Heading";
 
-function App() {
+export default function App() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console(JSON.stringify(data))
+    console.log(data)
+  };
+
   return (
-    <>
-      <div className="App">
-        <h1>Test</h1>
-      </div>
-      <Switch>
-        <Route path='/' render={() => <PatientEntry/>}></Route>
-      </Switch>
-    </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Heading />
+      <input {...register("firstName")} placeholder="First name" />
+      <input {...register("lastName")} placeholder="Last name" />
+      <select {...register("category")}>
+        <option value="">Reason for visit...</option>
+        <option value="Covid">Covid</option>
+        <option value="General">General</option>
+        <option value="Neurological">Neurological</option>
+        <option value="Respitory">Respitory</option>
+        <option value="Cardiovascular">Cardiovascular</option>
+      </select>
+      <input type="submit" />
+    </form>
   );
 }
+// <-- USING-FORM-HOOK-END -->
 
-export default App;
+
+// // <-- USING-STATE-HOOK-START -->
+// import React, {useState} from "react";
+// import Heading from "./Heading";
+
+// export default function App() {
+//   const [ form, setForm ] = useState({
+//     firstName: '',
+//     lastName: '',
+//     category: ''
+//   });
+//   const { firstName, lastName, category } = form
+  
+//   const handleChange = e => {
+//     setForm({
+//       ...form,
+//       [e.target.name]: e.target.value,
+//     });
+//   }
+
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//     console.log(form)
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <Heading />
+//       <input name="firstName" placeholder="First name" value={ firstName } onChange={handleChange}/>
+//       <input name="lastName" placeholder="Last name" value={ lastName } onChange={handleChange}/>
+//       <select name="category" value={category} onChange={handleChange}>
+//         <option value="">Reason for visit...</option>
+//         <option value="Covid">Covid</option>
+//         <option value="General">General</option>
+//         <option value="Neurological">Neurological</option>
+//         <option value="Respitory">Respitory</option>
+//         <option value="Cardiovascular">Cardiovascular</option>
+//       </select>
+//       <input type="submit" />
+//     </form>
+//   );
+// }
+// // <-- USING-STATE-END -->
---
+++ frontend/src/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default () => {
+
+  return (
+    <div style={{ marginBottom: 30 }}>
+      <h1 className="h1">
+        Patient Admission Form
+      </h1>
+      <p style={{ fontSize: 14, lineHeight: 1.3 }}>
+        Fill out the form to get added as a patient.
+      </p>
+    </div>
+  );
+};
