16bb0f655f7ae9e3889776f964b74e999c2016e9
Emmanuel <emmanuelamon92@gmail.com>
2021-07-29 12:30:55 -0400

    starting to add function to post user info to dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
-// <-- USING-FORM-HOOK-START -->
-import * as React from "react";
+// <--- USING FORM HOOK START --->
+
+
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import Heading from "./Heading";
 
 export default function App() {
+  //USEFORM HOOK WITH DATA BEING PASSED TO STATE IN ORDER TO BE PLACED IN POST REQUEST FETCH
   const { register, handleSubmit } = useForm();
 
-  const onSubmit = (data) => {
-    alert(JSON.stringify(data))
-    console.log(data)
-  };
+  const [userInfo, setUserInfo] = useState({})
+  const onSubmit = (data) => setUserInfo(data);
+  console.log(userInfo)
+
+
+//<--- DELETE REQUEST DELETING PATIENT FROM PORT:9292 /PATIENTS/ ENDPOINT START --->
+
 
 //   const handleClick = () => {
 //     fetch("http://localhost:9292/patients/"+ patient.id, {
@@ -21,36 +27,70 @@ export default function App() {
 //     updateCourses(course.id)
 // }
 
-// fetch("http://localhost:9292/patients", {
-//   method: 'POST',
-//   headers: {
-//       "Content-Type": "application/json",
-//       "Accepts": "application/json"
-//   },
-// body: JSON.stringify({first_name: "Jim", last_name: "Dean", condition: "General"})
-// }).then(res => res.json()).then(data => console.log(data))
+
+//<--- DELETE REQUEST DELETING PATIENT FROM PORT:9292 /PATIENTS/ ENDPOINT END --->
+
+  
+//<--- POST REQUEST FROM USER INPUT FORM TO DATABASE PORT:9292 /PATIENTS/ ENDPOINT START ---> 
+  
+  
+fetch("http://localhost:9292/patients/", {
+  method: 'POST',
+  headers: {
+      "Content-Type": "application/json",
+      "Accepts": "application/json"
+  },
+body: JSON.stringify({first_name: userInfo.firstName, last_name: userInfo.lastName, condition: userInfo.condition})
+}).then(res => res.json()).then(data => {
+  console.log(data)
+})
+  
+  
+//<--- POST REQUEST FROM USER INPUT FORM TO DATABASE PORT:9292 /PATIENTS/ ENDPOINT END ---> 
+  
+  
+// <--- FUNCTION POSTING PATIENT INFO TO DOM START --->
+  
+
+  const postPatientToDom = () => {
+    return (
+      <div className='user-card'>
+        <h3>First Name: { userInfo.firstName }</h3>
+        <h3>Last Name: { userInfo.lastName }</h3>
+        <h3>Condition: { userInfo.condition }</h3>
+      </div>
+    )}
+
+
+ //<--- FUNCTION POSTING PATIENT INFO TO DOM END ---> 
+  
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Heading />
-      <input {...register("firstName")} placeholder="First name" />
-      <input {...register("lastName")} placeholder="Last name" />
-      <select {...register("category")}>
-        <option value="">Reason for visit...</option>
-        <option value="Covid">Covid</option>
-        <option value="General">General</option>
-        <option value="Neurological">Neurological</option>
-        <option value="Respitory">Respitory</option>
-        <option value="Cardiovascular">Cardiovascular</option>
-      </select>
-      <input type="submit" />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Heading />
+        <input {...register("firstName")} placeholder="First name" autoComplete="off"/>
+        <input {...register("lastName")} placeholder="Last name" autoComplete="off"/>
+        <select {...register("condition")}>
+          <option value="">Reason for visit...</option>
+          <option value="Covid">Covid</option>
+          <option value="General">General</option>
+          <option value="Neurological">Neurological</option>
+          <option value="Respitory">Respitory</option>
+          <option value="Cardiovascular">Cardiovascular</option>
+        </select>
+        <input type="submit" />
+      </form>
+      <div className='patient-data'>{postPatientToDom()}</div>
+    </div>
   );
 }
-// <-- USING-FORM-HOOK-END -->
 
 
-// // <-- USING-STATE-HOOK-START -->
+// <--- USING FORM HOOK END --->
+
+
+// // <--- USING STATE HOOK START --->
 // import React, {useState} from "react";
 // import Heading from "./Heading";
 
@@ -58,9 +98,9 @@ export default function App() {
 //   const [ form, setForm ] = useState({
 //     firstName: '',
 //     lastName: '',
-//     category: ''
+//     condition: ''
 //   });
-//   const { firstName, lastName, category } = form
+//   const { firstName, lastName, condition } = form
   
 //   const handleChange = e => {
 //     setForm({
@@ -79,7 +119,7 @@ export default function App() {
 //       <Heading />
 //       <input name="firstName" placeholder="First name" value={ firstName } onChange={handleChange}/>
 //       <input name="lastName" placeholder="Last name" value={ lastName } onChange={handleChange}/>
-//       <select name="category" value={category} onChange={handleChange}>
+//       <select name="condition" value={condition} onChange={handleChange}>
 //         <option value="">Reason for visit...</option>
 //         <option value="Covid">Covid</option>
 //         <option value="General">General</option>
